6a5aee9aa327ebc1406d10cdc964f75aba0ab8f0
Venu Raidu <vraidu@qti.qualcomm.com>
2024-11-20 14:05:53 +0530

    Added QNN based Object Detection Android Application
    Signed-off-by: quic-vraidu <quic_vraidu@quicinc.com>

---
+++ Solutions/QNN/VisionSolution1-ObjectDetection-YoloNas/app/src/main/java/com/qcom/aistack_objdetect/RectangleBox.java
@@ -0,0 +1,32 @@
+//============================================================================
+// Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+//============================================================================
+
+package com.qcom.aistack_objdetect;
+
+import java.util.ArrayList;
+/**
+ * RectangleBox class defines the property associated with each box like coordinates
+ * labels, confidence etc.
+ * Can also create copy of boxes.
+ */
+public class RectangleBox {
+
+    public float top;
+    public float bottom;
+    public float left;
+    public float right;
+
+    public int fps;
+    public String processing_time;
+    public String label;
+    public static ArrayList<RectangleBox> createBoxes(int num) {
+        final ArrayList<RectangleBox> boxes;
+        boxes = new ArrayList<>();
+        for (int i = 0; i < num; ++i) {
+            boxes.add(new RectangleBox());
+        }
+        return boxes;
+    }
+}
